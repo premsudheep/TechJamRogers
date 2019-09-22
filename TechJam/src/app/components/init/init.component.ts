14be0e491bbdc6ecbe921636c0002d1fb32cd4e2
prem <sudheep.prem@gmail.com>
2019-09-22 10:39:00 -0400

    first functional UI

--- TechJam/src/app/components/init/init.component.ts
+++ TechJam/src/app/components/init/init.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Store} from "@ngrx/store";
+import {InitialAction} from "../../State/init.action";
+import {Subscription} from "rxjs/index";
 
 @Component({
   selector: 'app-init',
   templateUrl: './init.component.html',
   styleUrls: ['./init.component.scss']
 })
-export class InitComponent implements OnInit {
+export class InitComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  resSub: Subscription;
+  responsePayload = {};
+
+  ngOnDestroy(): void {
+    this.resSub.unsubscribe();
+  }
+
+  constructor(private store: Store<any>) { }
 
   ngOnInit() {
+
+    const searchPayload = {
+      "device": {
+        deviceType:"phone",
+      },
+      "search":"",
+      "postalCode":""
+    }
+
+    this.store.dispatch(new InitialAction(searchPayload));
+    this.resSub = this.store.select('initValue').subscribe(res => {
+      this.responsePayload = res.initValue;
+      console.log(this.responsePayload);
+    })
   }
 
 }
